87d8099ac39fc54c66a1b90a87204cb9489a9e77
Anna Lewis <anna.frances.lewis@gmail.com>
2016-07-18 20:22:30 -0600

    initial commit for anna's closet

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex');
+var pg = require('pg');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Anna's Closet" });
+});
+
+module.exports = router;
